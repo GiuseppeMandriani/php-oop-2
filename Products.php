9edf3bc54f0f8e5666bbc549985df0bc4ed7975b
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-06-01 16:06:39 +0200

    start Products

---
+++ Products.php
@@ -0,0 +1,29 @@
+<?php 
+
+/**
+ * CLASSE PRODUCTS (Classe Padre)
+ */
+
+class Products {
+    // Attributi/Proprietà/Valori
+    protected $type;
+    protected $brand;
+    protected $price;
+
+
+
+    // Costruct per inizio
+    
+    function __construct($type,$brand,$price){
+        $this->type = $type;
+        $this->brand = $brand;
+        $this->price = $price;
+    }
+
+    // Methods
+}
+
+
+
+
+
---
+++ Shoes.php
@@ -0,0 +1,39 @@
+<?php 
+// Import Class Products
+
+require_once __DIR__ . '/Products.php';
+
+/**
+*   CLASS SHOES (Classe Figlia)
+*/
+
+class Shoes extends Products {
+    // Attributi/Proprietà
+    protected $name;
+    protected $descriptions;
+    protected $review;
+
+
+    //Construct
+
+    function __construct($type,$brand,$price,$name,$descriptions,$review){
+        parent::__construct($type,$brand,$price);
+        $this->name = $name;
+        $this->descriptions = $descriptions;
+        $this->review = $review;
+    }
+
+
+
+    //Methods
+}
+
+// Istanze
+
+$model_1 = new Shoes('Sneakers', 'D&G', 250, 'Sneakers-Portofino', 'Scarpa in vera pelle...', 5);
+
+var_dump($model_1);
+
+$model_2 = new Shoes('Stringate', 'D&G', 250, 'Derby Intreccio a mano', 'Scarpa in vera pelle...', 5);
+
+var_dump($model_2);
